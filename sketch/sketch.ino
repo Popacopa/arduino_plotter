25c765bdc15c8aa9ae8943b725b22262b5602602
pavlider <118076297+Popacopa@users.noreply.github.com>
2023-05-26 21:00:41 +0300

    arduino

---
+++ sketch/sketch.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    switch (Serial.read()){
+      case 'f': digitalWrite(4, LOW);
+        break;
+      case 'n': digitalWrite(4, HIGH);
+        break;
+  }
+  }
+   
+}
