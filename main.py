c96aab3bbb5886db793eb6753745e998e542cfdd
pavlider <118076297+Popacopa@users.noreply.github.com>
2023-05-26 20:59:36 +0300

    etc

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import typing
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtCore import QIODevice
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from sys import argv, exit
 import plotWindow
 import port
-
+res = {}
 class MainWindow(QMainWindow, plotWindow.Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
         self.pushButton_2.clicked.connect(self.openPort)
+        self.ports = {port.portName(): port.description() for port in QSerialPortInfo.availablePorts()}
+        self.ports = list(filter(self.__func, self.ports))
+        self.comboBox.addItems(self.ports)
+        self.checkBox.clicked.connect(self.check)
+    def check(self):
+        match self.checkBox.checkState():
+            case 2:serial.write('4n'.encode())
+            case 0:serial.write('f'.encode())
+        print('n'.encode())
+    def __func(self, description):
+        return True if 'USB' in self.ports[description] else False
     def openPort(self):
         global view 
-        view = GraphView('port1')
-        view.show()
+        global serial
+        serial = QSerialPort()
+        serial.BaudRate(9600)
+        serial.setPortName(self.comboBox.currentText())
+        serial.open(QIODevice.ReadWrite)
+        #view = GraphView('port1')
+        #view.show()
 class GraphView(QMainWindow, port.Ui_MainWindow):
     def __init__(self, name) -> None:
         super().__init__()
