6068b1fcd92a2b70db4ba0591e9033d99445819d
pavel <paushakow@gmail.com>
2023-05-28 22:04:32 +0300

    etc

--- main.py
+++ main.py
@@ -8,24 +8,25 @@ import port
 
 
 res = {}
+
 class MainWindow(QMainWindow, plotWindow.Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
-        self.pushButton_2.clicked.connect(self.openPort)
+        self.pushButton_2.clicked.connect(self.startSerialPort)
         self.ports = {port.portName(): port.description() for port in QSerialPortInfo.availablePorts()}
-        self.ports = list(filter(self.__func, self.ports))
+        def __isUSB(description):
+            return True if 'USB' in self.ports[description] else False
+        self.ports = list(filter(__isUSB, self.ports))
         self.comboBox.addItems(self.ports)
         self.checkBox.clicked.connect(self.check)
         self.checkBox_2.clicked.connect(self.check_2)
         self.checkBox_3.clicked.connect(self.check_3)
         self.checkBox_4.clicked.connect(self.check_4)
-    def printIn(self):
-        In = serial.readLine()
-        print(In)
-        #self.on = self.to_bytes(41)
-        #self.off = self.to_bytes(40)
-    def to_bytes(self, n):...
+        self.pushButton.setEnabled(False)
+    def __printData(self):...
+        #In = serial.readLine()
+        #print(In)
     def check(self):
         match self.checkBox.checkState():
             case 2:serial.write(bytes([4, 1])); #print(bytes([4, 1]))
@@ -42,16 +43,16 @@ class MainWindow(QMainWindow, plotWindow.Ui_MainWindow):
          match self.checkBox_4.checkState():
             case 2:serial.write(bytes([7, 1])); #print(bytes([4, 1]))
             case 0:serial.write(bytes([7, 0]));
-    def __func(self, description):
-        return True if 'USB' in self.ports[description] else False
-    def openPort(self):
+    def startSerialPort(self):
         #global view 
         global serial
         serial = QSerialPort()
         serial.BaudRate(9600)
         serial.setPortName(self.comboBox.currentText())
         serial.open(QIODevice.ReadWrite)
-        serial.readyRead.connect(self.printIn)
+        serial.readyRead.connect(self.__printData)
+        self.pushButton.setEnabled(True)
+        self.pushButton_2.setEnabled(False)
         #view = GraphView('port1')
         #view.show()
 class GraphView(QMainWindow, port.Ui_MainWindow):
